22e302220b22e39819fb1fc714af85c9ca55ba52
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-09-27 15:21:26 -0400

    fix/ constants moved to util folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import {initialCards, selectors, validationSettings, cardAddForm, profileEditForm, cardListEl}
-      from "../components/constants.js";
+      from "../utils/constants.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from '../components/UserInfo.js';
